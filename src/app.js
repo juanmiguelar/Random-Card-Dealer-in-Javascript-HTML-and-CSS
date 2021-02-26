b737c60635e1a77741ca61119c4148b63eebf9ed
Juan Miguel <juan.ariasmejias@ucr.ac.cr>
2021-02-26 02:49:37 +0000

    Ya se generan las cartas de manera aleatoria

--- src/app.js
+++ src/app.js
@@ -7,5 +7,53 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let house = ["diamond", "heart", "spade", "club"];
+  let values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let randomHouse = GetRandomValue(house);
+  let randomValue = GetRandomValue(values);
+  let cardElement = document.getElementById("card");
+  let leftSymbol = this.document.getElementById("leftSymbol");
+  let rightSymbol = this.document.getElementById("rightSymbol");
+  let number = this.document.getElementById("number");
+
+  cardElement.className = "card " + randomHouse;
+  number.innerHTML = randomValue;
+
+  switch (randomHouse) {
+    case "diamond":
+      leftSymbol.innerHTML = rightSymbol.innerHTML = "&#9830;";
+      break;
+    case "heart":
+      leftSymbol.innerHTML = rightSymbol.innerHTML = "&#9829;";
+      break;
+    case "spade":
+      leftSymbol.innerHTML = rightSymbol.innerHTML = "&#9824;";
+      break;
+    case "club":
+      leftSymbol.innerHTML = rightSymbol.innerHTML = "&#9827;";
+      break;
+
+    default:
+      break;
+  }
+
+  function GetRandomValue(house) {
+    let randomIndex = Math.floor(Math.random() * (house.length - 1));
+
+    return house[randomIndex];
+  }
 };
